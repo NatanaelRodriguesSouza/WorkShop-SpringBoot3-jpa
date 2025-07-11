fb82d1b47380eeccfeae4490a5cf951165c80dfa
Natanael Rodrigues <natanaelrodriguessouza13@gmail.com>
2025-07-10 21:11:28 -0300

    User entity and resource

---
+++ src/main/java/com/ProjetoWeb/WebProject/resource/UserResource.java
@@ -0,0 +1,19 @@
+package com.ProjetoWeb.WebProject.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ProjetoWeb.WebProject.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1,"Maria","maria@gmail.com","99999","00000");
+		return ResponseEntity.ok().body(u);
+	}
+}
